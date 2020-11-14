c7e4f84f5925ca7eba65a097f61e37359d572a0f
j.nz <j.nizeyimana@net.estia.fr>
2020-11-14 12:50:05 +0100

    ArtcicleAdapter

--- app/src/main/java/com/example/newsletter/MainActivity.kt
+++ app/src/main/java/com/example/newsletter/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.newsletter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.example.newsletter.fragments.HomeFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        NavigationListener {
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        toolbar = findViewById(R.id.toolbar)
+        showFragment(HomeFragment())
     }
+
+    override fun showFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+        }.commit()
+    }
+
+    override fun showFragmentinFragment(id:Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(id, fragment)
+            addToBackStack(null)
+        }.commit()
+    }
+
+    override fun updateTitle(title: Int) {
+        toolbar.setTitle(title)
+    }
+
 }
--- app/src/main/java/com/example/newsletter/fragments/ListArticlesFragment.kt
+++ app/src/main/java/com/example/newsletter/fragments/ListArticlesFragment.kt
@@ -18,68 +18,78 @@ import com.example.newsletter.adapter.ArticleDetailsAdapter
 import com.example.newsletter.adapter.ListArticlesHandler
 import com.example.newsletter.data.ArticleRepository
 import com.example.newsletter.models.Article
+import com.example.newsletter.models.ArticleResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class ListArticlesFragment : Fragment(), ListArticlesHandler {
+class ListArticlesFragment (subject: String): Fragment(), ListArticlesHandler {
     private lateinit var recyclerView: RecyclerView
-
+    val subject = subject
     /**
      * Fonction permettant de définir une vue à attacher à un fragment
      */
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.list_articles_fragment, container, false)
         recyclerView = view.findViewById(R.id.articles_list)
         recyclerView.layoutManager = LinearLayoutManager(context)
         recyclerView.addItemDecoration(
-            DividerItemDecoration(
-                requireContext(),
-                DividerItemDecoration.VERTICAL
-            )
+                DividerItemDecoration(
+                        requireContext(),
+                        DividerItemDecoration.VERTICAL
+                )
         )
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getArticles(subject)
+    }
+    /**
+     * Récupère la liste des articles dans un thread secondaire
+     */
+    private fun getArticles(subject: String) {
         lifecycleScope.launch(Dispatchers.IO) {
-            val articles = ArticleRepository.getInstance().getArticles()
+            val articles = ArticleRepository.getInstance().getArticles(subject)
             bindData(articles)
         }
     }
-        /**
-         * Récuperation  de la liste des articles dans un thread secondaire
-         */
 
-        private fun getArticles() {
-            lifecycleScope.launch(Dispatchers.IO) {
-                val articles = ArticleRepository.getInstance().getArticles()
-            }
+    /**
+     * Rempli le recyclerview avec les données récupérées dans le web service
+     * Cette action doit s'effectuer sur le thread principale
+     */
+    private fun bindData(articles: ArticleResponse) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            //créer l'adapter
+            //associer l'adapteur au recyclerview
+            val adapter = ArticleAdapter(articles,this@ListArticlesFragment)
+            recyclerView.adapter = adapter
         }
+    }
 
-        /**
-         * Remplir le recyclerview avec les données récupérées dans le web service
-         * Cette action doit s'effectuer sur le thread principale
-         */
-        private fun bindData (articles: List<Article>) {
-            lifecycleScope.launch(Dispatchers.Main) {
-                val adapter = ArticleAdapter(articles)
-                recyclerView.adapter = adapter
-            }
-
-            override fun showArticle(article: Article) {
-
-            }
-
-            override fun back() {
-            }
-
-            override fun showPage(url: String) {
-            }
+    override fun showArticle(article: Article) {
+        (activity as? NavigationListener)?.let {
+            it.updateTitle(R.string.article_details)
         }
+        val adapter = ArticleDetailsAdapter(article,this)
+        recyclerView.adapter = adapter
+    }
 
+    override fun back() {
+        (activity as? NavigationListener)?.let {
+            it.updateTitle(R.string.list_articles)
+        }
+        getArticles(subject)
+    }
+
+    override fun showPage(url: String) {
+        val chemin: Uri = Uri.parse(url)
+        val naviguer = Intent(Intent.ACTION_VIEW,chemin)
+        startActivity(naviguer)
+    }
 }
